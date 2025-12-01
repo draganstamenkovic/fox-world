e931c933e5527a17a50166d6d62a675c611175b8
CodeLazarus <draganstamenkovic@outlook.com>
2025-12-01 16:50:25 +0100

    implented following camera implented parallax effect

--- Assets/Scripts/Configs/BackgroundConfig.cs
+++ Assets/Scripts/Configs/BackgroundConfig.cs
@@ -5,7 +5,9 @@ namespace Configs
     [CreateAssetMenu(fileName = "BackgroundConfig", menuName = "Configs/BackgroundConfig")]
     public class BackgroundConfig : ScriptableObject
     {
-        public GameObject skyBackground;
-        public GameObject forestBackground;
+        [Tooltip("Multiplier for forest X axis while following camera on X axis")]
+        public float forestXSpeedMultiplier = 1.05f;
+        [Tooltip("Multiplier for forest Y axis when following camera on Y axis")]
+        public float forestYSpeedMultiplier = 0.5f;
     }
 }
--- Assets/Scripts/Gameplay/Background/BackgroundManager.cs
+++ Assets/Scripts/Gameplay/Background/BackgroundManager.cs
@@ -1,3 +1,5 @@
+using System;
+using Cameras;
 using Configs;
 using UnityEngine;
 using VContainer;
@@ -7,15 +9,25 @@ namespace Gameplay.Background
     public class BackgroundManager : MonoBehaviour
     {
         [Inject] private readonly BackgroundConfig _config;
-
-        [SerializeField] private Transform forestTransform;
+        [Inject] private readonly ICameraManager _mainCamera;
+        [SerializeField] private Transform forrestTransform;
         [SerializeField] private Transform skyTransform;
         private Vector3 _cameraPosition;
-        /*
+        private float _forrestYAxis;
+        private void Start()
+        {
+            _forrestYAxis = forrestTransform.position.y;
+        }
+
         private void Update()
         {
-            skyTransform.transform.position = _cameraPosition;
+            _cameraPosition = _mainCamera.GetCameraPosition();
+            _cameraPosition.z = 0f;
+            skyTransform.position = _cameraPosition;
+            forrestTransform.position = new Vector3(
+                _cameraPosition.x * _config.forestXSpeedMultiplier,
+                _forrestYAxis + _cameraPosition.y * +_config.forestYSpeedMultiplier,
+                0);
         }
-        */
     }
 }
