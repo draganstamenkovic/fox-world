bc81f6c7029a407cb89595dde7abbc404b78f1e1
CodeLazarus <draganstamenkovic@outlook.com>
2025-12-04 18:08:02 +0100

    Add all similar enemy logic to enemy class

--- Assets/Scripts/Gameplay/Enemies/Enemy.cs
+++ Assets/Scripts/Gameplay/Enemies/Enemy.cs
@@ -1,19 +1,53 @@
 using UnityEngine;
+using DG.Tweening;
 
 namespace Gameplay.Enemies
 {
     public class Enemy : MonoBehaviour
     {
         [SerializeField] private Animator animator;
-        [SerializeField] private CircleCollider2D circleCollider;
-
-        protected bool StopMovement { get; private set; }
-        public virtual void Die()
+        [SerializeField] private Collider2D enemyCollider;
+        [SerializeField] private Transform enemyTransform;
+        [SerializeField] private Transform startPointTransform;
+        [SerializeField] private Transform endPointTransform;
+        
+        public float enemyMoveDuration = 2f;
+        private bool _stopMovement;
+        private void Start()
         {
-            StopMovement = true;
+            Move();
+        }
+        
+        public void Die()
+        {
+            enemyTransform.DOKill();
+            _stopMovement = true;
             animator.Play(AnimationIds.EnemyDied);
-            circleCollider.enabled = false;
+            enemyCollider.enabled = false;
             Destroy(gameObject, 0.5f);
+        }
+        
+        private void Move()
+        {
+            if (_stopMovement) return;
+            
+            enemyTransform.DOMove(endPointTransform.position, enemyMoveDuration)
+                .SetEase(Ease.Linear)
+                .OnComplete(() =>
+                {
+                    if (_stopMovement) return;
+                    
+                    enemyTransform.localScale = new Vector3(-1, 1, 1);
+                    enemyTransform.DOMove(startPointTransform.position, enemyMoveDuration)
+                        .SetEase(Ease.Linear)
+                        .OnComplete(() => 
+                        { 
+                            if (_stopMovement) return;
+                            
+                            enemyTransform.localScale = Vector3.one;
+                            Move();
+                        });
+                });
         }
     }
 }
--- Assets/Scripts/Gameplay/Enemies/Vulture.cs
+++ Assets/Scripts/Gameplay/Enemies/Vulture.cs
@@ -1,49 +1,7 @@
-using Configs;
-using UnityEngine;
-using DG.Tweening;
-
 namespace Gameplay.Enemies
 {
     public class Vulture : Enemy
     {
-        [SerializeField] private Transform vultureTransform;
-        [SerializeField] private Transform startPointTransform;
-        [SerializeField] private Transform endPointTransform;
-        [SerializeField] private EnemiesConfig enemiesConfig;
         
-        private void Start()
-        {
-            Move();
-        }
-
-        public override void Die()
-        {
-            // Kill all tweens on this transform
-            vultureTransform.DOKill();
-            base.Die();
-        }
-
-        private void Move()
-        {
-            if (StopMovement) return;
-            
-            vultureTransform.DOMove(endPointTransform.position, enemiesConfig.vultureMoveDuration)
-                .SetEase(Ease.Linear)
-                .OnComplete(() =>
-                {
-                    if (StopMovement) return;
-                    
-                    vultureTransform.localScale = new Vector3(-1, 1, 1);
-                    vultureTransform.DOMove(startPointTransform.position, enemiesConfig.vultureMoveDuration)
-                        .SetEase(Ease.Linear)
-                        .OnComplete(() => 
-                        { 
-                            if (StopMovement) return;
-                            
-                            vultureTransform.localScale = Vector3.one;
-                            Move();
-                        });
-                });
-        }
     }
 }
